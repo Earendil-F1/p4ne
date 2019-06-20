0065cecb7dc69c6bef0d38c4f4b69b3202852943
Earendil-F1 <Earendil.F1@gmail.com>
2019-06-20 19:59:24 +0300

    initial

--- Lab1.4/subnets_generator.py
+++ Lab1.4/subnets_generator.py
@@ -1,18 +1,16 @@
 from ipaddress import IPv4Network
-import random
+from random import randint
 
 IPADDR_FIRST_OCTET_START = 11
 IPADDR_FIRST_OCTET_END = 223
 NETS_NUMBER = 100
 
 def prefix_generator():
-    oc = list()
-    oc.append(random.randint(IPADDR_FIRST_OCTET_START, IPADDR_FIRST_OCTET_END))
-    for i in range (1,3):
-        oc.append(random.randint(0, 255))
-    oc.append(0)
-    pref =  '%s.%s.%s.%s' % (oc[0], oc[1], oc[2], oc[3])
-    mask = random.randint(8, 24)
+    pref =  '%s.%s.%s.%s' % (randint(IPADDR_FIRST_OCTET_START, IPADDR_FIRST_OCTET_END),
+                             randint(0, 255),
+                             randint(0, 255),
+                             0)
+    mask = randint(8, 24)
     return (IPv4Network ((pref, mask), strict=False)) # Automatically converts host bits to 0s
 
 class IPv4RandomNetwork(IPv4Network):
