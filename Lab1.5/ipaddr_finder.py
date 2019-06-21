b08f2d3ade1d6cf7a6f9e292be7a5f8048ccaaae
Earendil-F1 <Earendil.F1@gmail.com>
2019-06-21 19:58:48 +0300

    initial

--- Lab1.5/ipaddr_finder.py
+++ Lab1.5/ipaddr_finder.py
@@ -8,7 +8,7 @@ for f in flist:
     with open(f) as file:
         for line in file:
             if ('ip address' in line and '.' in line):
-                if ((not 'dhcp' in line) and (not 'no' in line)):
+                if (('dhcp' not in line) and ('no' not in line)):
                     line = line.strip('\r\n')
                     if (line not in target):
                         print (line)
